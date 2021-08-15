5648c7714e9ff3f8fc3ba3f5051e74ddc64fe063
moustapharamadan <moustapharamadan@hotmail.com>
2021-08-15 19:49:53 +0300

    Deploy Backend with Heroku

---
+++ apiServer.js
@@ -0,0 +1,54 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const path = require("path");
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults({
+  static: "node_modules/json-server/public",
+});
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+server.use((req, res, next) => {
+  setTimeout(next, 0);
+});
+
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
+  }
+  next();
+});
+
+server.post("/courses/", function (req, res, next) {
+  const error = validateCourse(req.body);
+  console.log("Error server" + error);
+  if (error) {
+    res.status(400).send(error);
+  } else {
+    req.body.slug = createSlug(req.body.title); // Generate a slug for new courses.
+    next();
+  }
+});
+
+server.use(router);
+
+const port = 3001;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
+
+function validateCourse(course) {
+  console.log("Server side" + course);
+  if (!course.title) return "Title is required.";
+  if (!course.authorId) return "Author is required.";
+  if (!course.category) return "Category is required.";
+  return "";
+}
+
+function createSlug(value) {
+  return value
+    .replace(/[^a-z0-9_]+/gi, "-")
+    .replace(/^-|-$/g, "")
+    .toLowerCase();
+}
---
+++ createMockDb.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+const mockData = require("./mockData");
+
+const { courses, authors } = mockData;
+const data = JSON.stringify({ courses, authors });
+const filePath = path.join(__dirname, "db.json");
+fs.writeFile(filePath, data, function (err) {
+  err ? console.log(err) : console.log("Mock DB Created");
+});
